e0c8fd75cdf8ed72e7d7e8813edd3a34a85e10a4
liululu <383449174@qq.com>
2021-11-22 12:40:45 +0800

    finished

--- app/api/dir.js
+++ app/api/dir.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const path = require("path")
+const send = require("koa-send")
 //用户路由
 const Router = require('@koa/router')
 const router = new Router()
@@ -9,23 +10,31 @@ const FILES_BASE_PATH = "./resources"
 //统一路径前缀
 router.prefix("/api")
 
-// 提供路径API接口
+// 提供路径API接口和资源文件
 router.get('/dir/:pathMatch(.*)*',async ctx => {
     let tgt_path = path.join(FILES_BASE_PATH, ctx.params.pathMatch === undefined ? '' : ctx.params.pathMatch)
     let validated_path = await validate_path(tgt_path)
-    
-    ctx.type = "JSON"
-    if(validated_path === "not a valid directory" || !validated_path){
+
+    if(validated_path === "not a valid directory"){
+        ctx.type = "JSON"
         ctx.body = {
             code: -1,
             data: []
         }
-    }else{
+    }else if(!validated_path){
+        // 资源文件
+        ctx.attachment(tgt_path)
+        await send(ctx, tgt_path)
+    }
+    else{
+        ctx.type = "JSON"
+        // 文件夹
         ctx.body = {
             code: 1,
             data: read_dir_all_infos(tgt_path)
         }
     }
+
 })
 
 // 验证路径是否合法 - 若路径不存在或者路径是文件则判定为不合法
